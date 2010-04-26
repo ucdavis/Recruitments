a3044ecf8bc7f4c8c71e47a69295ddb6122ecec9
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:17:14 +0000

    added sorting to the get email templates method. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4180. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3269

--- Recruitment.BLL/TemplateTypeBLL.cs
+++ Recruitment.BLL/TemplateTypeBLL.cs
@@ -15,10 +15,14 @@ namespace CAESDO.Recruitment.BLL
             return TemplateTypeBLL.GetUniqueByExample(templateType);
         }
 
+        /// <summary>
+        /// Get just the templates that are email templates
+        /// </summary>
         public static List<TemplateType> GetEmailTemplates()
         {
             var emailTemplates = from t in EntitySet
                                  where t.IsEmailTemplate
+                                 orderby t.Type ascending 
                                  select t;
 
             return emailTemplates.ToList();
