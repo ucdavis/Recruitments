25bc7b6a0dc2ce4d443faf1123ac25627ad76570
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 20:54:03 +0000

    See task 247 (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 2250. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C2616

--- Recruitment.Test/StaticProperties.cs
+++ Recruitment.Test/StaticProperties.cs
@@ -10,6 +10,8 @@ namespace CAESDO.Recruitment.Test
         public static int ExistingProfileID = 11;
         public static int ExistingApplicationID = 11;
         public static int ExistingPositionID = 13;
+        public static int ExistingFileTypeID = 1;
+        public static int ExistingFileID = 1;
 
         public static string ExistingApplicantEmail = "srkirkland@ucdavis.edu";
         public static string TestString = "TEST";
