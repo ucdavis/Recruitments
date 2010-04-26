28c321fd27db76970b3c89229a2a2370110f410c
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:17:44 +0000

    Moved the template processing class to the core library and added in some tests around processing certain templates.  Added a test that failed with the old code because of an opening token but works now that the begindex now has >= 0. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4324. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3301

---
+++ Recruitment.Core/Abstractions/TemplateProcessing.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using CAESDO.Recruitment.Core.Domain;
+
+namespace CAESDO.Recruitment.Core.Abstractions
+{
+    public interface ITemplateProcessing
+    {
+        string ProcessTemplate(Reference reference, Application application, string template, bool includeUploadPortion);
+    }
+
+    /// <summary>
+    /// Summary description for TemplateProcessing
+    /// </summary>
+    public class TemplateProcessing : ITemplateProcessing
+    {
+        private const string STR_PositionDetailsURL = "/PositionDetails.aspx";
+        private const string STR_UploadReferenceURL = "/UploadReference.aspx";
+
+        private Application _application;
+        private bool _includeUploadPortion = false;
+        private Reference _reference;
+
+        public TemplateProcessing()
+        {
+        }
+
+        #region ITemplateProcessing Members
+
+        public string ProcessTemplate(Reference reference,
+                                      Application application, string template,
+                                      bool includeUploadPortion)
+        {
+            _includeUploadPortion = includeUploadPortion;
+
+            return ProcessTemplate(reference, application, template);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Iterates through the given template file and replaces any place where there is a template field marked with {}
+        /// </summary>
+        /// <param name="body">The template text.</param>
+        /// <returns>Body text with fields populated with correct values.</returns>
+        private string HandleBody(string body)
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("<html><body>");
+
+            string parameter;
+
+            int begindex = body.IndexOf("{"); // Find the beginning of a replacement string
+            int endindex;
+            while (begindex >= 0)
+            {
+                sb.Append(body.Substring(0, begindex));
+                // Copy the text that comes before the replacement string to temp
+                body = body.Substring(begindex); // Removes the first part of the string before the {
+
+                endindex = body.IndexOf("}"); // Find the end of a replacement string
+
+                parameter = body.Substring(0, endindex + 1); // Pulls the text between {}
+                body = body.Substring(endindex + 1); // removes the parameter substring
+
+                sb.Append(replaceParameter(parameter));
+
+                begindex = body.IndexOf("{"); // Find the beginning of a replacement string
+            }
+
+            sb.Append(body);
+
+            if (_includeUploadPortion)
+                sb.Append(getUploadIDPortion());
+
+            sb.Append("</body></html>");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Converts the parameter name into the correct value.
+        /// </summary>
+        /// <param name="parameter">Parameter name from the template.</param>
+        /// <returns>Value that should be put into the space where the field was.</returns>
+        private string replaceParameter(string parameter)
+        {
+            // Trim the {}
+            int length = parameter.Length;
+            parameter = parameter.Substring(1, length - 2);
+
+            switch (parameter)
+            {
+                case "ReferenceName":
+                    return _reference.FullName;
+                    //if (this._reference.MiddleName.ToString() != "") // If the field is null, the ToString() will make it a blank string.
+                    //    return this._reference.FirstName + " " + this._reference.MiddleName + " " + this._reference.LastName;
+                    //else
+                    //    return this._reference.FirstName + " " + this._reference.LastName;
+                case "ReferenceLastName":
+                    return _reference.LastName;
+                case "ReferenceTitle":
+                    return _reference.Title;
+                case "ApplicantName":
+                    if (string.IsNullOrEmpty(_application.AssociatedProfile.FullName.Trim()))
+                        return "Name Not Given";
+                    else
+                        return _application.AssociatedProfile.FullName;
+
+                    //if (this._application.AssociatedProfile.MiddleName.ToString() != "") // If the field is null, the ToString() will make it a blank string.
+                    //return this._application.AssociatedProfile.FirstName + " " + this._application.AssociatedProfile.MiddleName + " " + this._application.AssociatedProfile.LastName;
+                    //else
+                    //  return this._application.AssociatedProfile.FirstName + " " + this._application.AssociatedProfile.LastName
+                case "RecruitmentAdminName":
+                    return _application.AppliedPosition.HRRep;
+                case "RecruitmentAdminEmail":
+                    return string.Format("<a href='mailto:{0}'>{0}</a>", _application.AppliedPosition.HREmail);
+                case "PositionContact":
+                    return _application.AppliedPosition.HRRep;
+                case "PositionContactEmail":
+                    return string.Format("<a href='mailto:{0}'>{0}</a>", _application.AppliedPosition.HREmail);
+                    //return this._application.AppliedPosition.HREmail;
+                case "PositionContactPhone":
+                    return _application.AppliedPosition.HRPhone;
+                case "Deadline": //deadline and reviewDate are the same token
+                case "ReviewDate":
+                    return _application.AppliedPosition.Deadline.ToLongDateString();
+                case "PositionTitle":
+                    //return this._application.AppliedPosition.PositionTitle;
+                    return getPositionLink();
+                case "PositionLink":
+                    return getPositionLink();
+                case "PrimaryDepartment":
+                    if (_application.AppliedPosition.PrimaryDepartment.Unit != null)
+                        return _application.AppliedPosition.PrimaryDepartment.Unit.FullName;
+                    break;
+                case "UploadLink":
+                    _includeUploadPortion = false;
+                    //Don't include the default upload portion since we are including it manually here
+                    return getUploadIDPortion(); //now return the uploadID portion
+                case "Date":
+                    var date = new StringBuilder();
+                    DateTime dateTime = DateTime.Now;
+
+                    // Will convert the existing date into the following format : May 16, 2007
+                    // Instead of something like 5/16/2007
+                    date.Append(GetMonth(dateTime.Month));
+                    date.Append(" " + dateTime.Day);
+                    date.Append(", " + dateTime.Year);
+
+                    return date.ToString();
+                default:
+                    //return string.Empty;
+                    break;
+            }
+#if DEBUG
+            return "Error";
+#else
+            return string.Empty;
+#endif
+        }
+
+        private string getUploadIDPortion()
+        {
+            return "<a href='" +
+                   HttpContext.Current.Request.Url.GetComponents(UriComponents.SchemeAndServer, UriFormat.SafeUnescaped) +
+                   HttpContext.Current.Request.ApplicationPath + STR_UploadReferenceURL + "?ID=" +
+                   _reference.UploadID + "'>Click here to upload reference letter</a>";
+        }
+
+        private string getPositionLink()
+        {
+            return string.Format("<a href='{0}?PositionID={1}'>{2}</a>",
+                                 HttpContext.Current.Request.Url.GetComponents(UriComponents.SchemeAndServer,
+                                                                               UriFormat.SafeUnescaped) +
+                                 HttpContext.Current.Request.ApplicationPath + STR_PositionDetailsURL,
+                                 _application.AppliedPosition.ID,
+                                 _application.AppliedPosition.PositionTitle);
+        }
+
+        // Takes an int representation of Month and returns the string name
+        private string GetMonth(int month)
+        {
+            string date = "";
+
+            switch (month)
+            {
+                case 1:
+                    date = "January";
+                    break;
+                case 2:
+                    date = "February";
+                    break;
+                case 3:
+                    date = "March";
+                    break;
+                case 4:
+                    date = "April";
+                    break;
+                case 5:
+                    date = "May";
+                    break;
+                case 6:
+                    date = "June";
+                    break;
+                case 7:
+                    date = "July";
+                    break;
+                case 8:
+                    date = "August";
+                    break;
+                case 9:
+                    date = "September";
+                    break;
+                case 10:
+                    date = "October";
+                    break;
+                case 11:
+                    date = "November";
+                    break;
+                case 12:
+                    date = "December";
+                    break;
+                default:
+                    break;
+            }
+            return date;
+        }
+
+        #endregion
+
+        public string ProcessTemplate(Reference reference,
+                                      Application application, string template)
+        {
+            _reference = reference;
+            _application = application;
+
+            return HandleBody(template);
+        }
+    }
+}
---
+++ Recruitment.Test/BusinessTests/TemplateProcessingTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using CAESDO.Recruitment.Core.Abstractions;
+using CAESDO.Recruitment.Core.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAESDO.Recruitment.Test.BusinessTests
+{
+    [TestClass]
+    public class TemplateProcessingTests
+    {
+        private Reference reference;
+        private Application application;
+
+        [TestMethod]
+        public void CanProcessTemplateByReplacingRecruitmentAdmin()
+        {
+            const string templateBody = "Who is the {RecruitmentAdminName}";
+
+            var processing = new TemplateProcessing();
+
+            var templateResult = processing.ProcessTemplate(reference, application, templateBody);
+
+            var templateReplace = templateResult.Replace("{RecruitmentAdminName}", application.AppliedPosition.HRRep);
+
+            Assert.IsNotNull(templateResult);
+            Assert.AreEqual(templateReplace, templateResult);
+        }
+
+        [TestMethod]
+        public void CanProcessTemplateByReplacingReferenceName()
+        {
+            const string templateBody = "Who is the {ReferenceName}";
+
+            var processing = new TemplateProcessing();
+
+            var templateResult = processing.ProcessTemplate(reference, application, templateBody);
+
+            var templateReplace = templateResult.Replace("{ReferenceName}", reference.FullName);
+
+            Assert.IsNotNull(templateResult);
+            Assert.AreEqual(templateReplace, templateResult);
+        }
+
+        [TestMethod]
+        public void CanProcessTemplateWithOnlyToken()
+        {
+            const string templateBody = "{ReferenceName}";
+
+            var processing = new TemplateProcessing();
+
+            var templateResult = processing.ProcessTemplate(reference, application, templateBody);
+
+            var templateReplace = templateResult.Replace("{ReferenceName}", reference.FullName);
+
+            Assert.IsNotNull(templateResult);
+            Assert.AreEqual(templateReplace, templateResult);
+        }
+
+        [TestInitialize]
+        public void SetupProperties()
+        {
+            var previewDepartment = new Department
+            {
+                DepartmentFIS = "CHAN",
+                PrimaryDept = true,
+                Unit = new Unit { FullName = "Advanced Sciences" }
+            };
+
+            var previewReference = new Reference { FirstName = "Mike", MiddleName = "H", LastName = "Jones", Title = "Dr." };
+
+            var previewApplication = new Application
+            {
+                AssociatedProfile =
+                    new Profile { FirstName = "John", MiddleName = "P", LastName = "Smith" }
+            };
+
+            previewApplication.AppliedPosition = new Position
+            {
+                Deadline = DateTime.Now.AddDays(20),
+                PositionTitle = "Professor of Technology",
+                HRRep = "Jane Williams",
+                HREmail = "jwilliams@ucdavis.edu",
+                Departments = new List<Department> { previewDepartment }
+            };
+
+            EntityIdSetter.SetIdOf<int>(previewApplication.AppliedPosition, 1);
+
+            reference = previewReference;
+            application = previewApplication;
+        }
+    }
+}
