614383e4365047ebfb9c6b3bc1626273f0bb6875
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:18:23 +0000

    Added a CI helper to Recruitments (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5241. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3337

---
+++ Recruitment.Test/ContinuousIntegrationDeploymentHack.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace CAESDO.Recruitment.Test
+{
+    /// <summary>
+    /// NOTE: This is a hack to make sure that the spring and SQLLite assemblies are copied into the Test deployemnt directory
+    /// This class never gets called.
+    /// </summary>
+    public class ContinuousIntegrationDeploymentHack
+    {
+        public ContinuousIntegrationDeploymentHack()
+        {
+            new System.Data.SQLite.SQLiteException();
+
+            throw new Exception("This class should never be called or instantiated");
+        }
+    }
+
+}
