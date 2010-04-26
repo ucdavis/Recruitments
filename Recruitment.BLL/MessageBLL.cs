54f56f7da83f54a0994bf9be99a9d426b2d1c196
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:17:02 +0000

    Started writing a message bll which includes an IMessageGateWay for sending out emails.  I think the message gateway is fully functional but it has not been tested, and no code yet calls it. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4150. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3256

---
+++ Recruitment.BLL/MessageBLL.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Mail;
+using CAESDO.Recruitment.Core.Domain;
+using System.Security.Principal;
+using System;
+
+namespace CAESDO.Recruitment.BLL
+{
+    public class MessageBLL
+    {
+        public static IMessageGateWay messageGateway = new MessageGateWay();
+
+
+    }
+
+    public interface IMessageGateWay
+    {
+        bool SendMessage(string to, string from, string body, string subject, IPrincipal currentUser);
+    }
+
+    public class MessageGateWay : IMessageGateWay
+    {
+        /// <summary>
+        /// Sends a message according to the given criteria
+        /// </summary>
+        /// <returns>true on success</returns>
+        public bool SendMessage(string to, string from, string body, string subject, IPrincipal currentUser)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                MailMessage message = new MailMessage(from, to, subject, body);
+                
+                message.IsBodyHtml = true;
+                
+                client.Send(message);
+            }
+            catch
+            {
+                return false; //False when message sending doesn't succeed
+            }
+
+            //Message is sent, record the event
+            using (var ts = new TransactionScope())
+            {
+                var messageTracking = new MessageTracking
+                                          {
+                                              To = to,
+                                              From = from,
+                                              Body = body,
+                                              DateSent = DateTime.Now,
+                                              SentBy = currentUser.Identity == null ? "N/A" : currentUser.Identity.Name
+                                          };
+
+                MessageTrackingBLL.EnsurePersistent(messageTracking);
+
+                ts.CommitTransaction();
+            }
+
+            return true; //Success
+        }
+    }
+}
---
+++ Recruitment.BLL/MessageTrackingBLL.cs
@@ -0,0 +1,7 @@
+﻿using CAESDO.Recruitment.Core.Domain;
+namespace CAESDO.Recruitment.BLL
+{
+    public class MessageTrackingBLL : GenericBLL<MessageTracking,int>
+    {
+    }
+}
