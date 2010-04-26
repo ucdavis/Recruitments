ca65e39bb978e1a3de7dc38aa1d80581eff84cd2
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:13:25 +0000

    Created a view applications shared user web control, and then modified the existing view applications pages to use this control. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3330. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3099

--- Recruitment.Web/Review/viewApplications.aspx.cs
+++ Recruitment.Web/Review/viewApplications.aspx.cs
@@ -1,87 +1,8 @@
-using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
-using CAESDO.Recruitment.Core.Domain;
-using CAESDO.Recruitment.BLL;
 
 namespace CAESDO.Recruitment.Web
 {
     public partial class Authorized_viewApplications : ApplicationPage
     {
-        private const string STR_PositionID = "PositionID";
-        private const string STR_ApplicationReviewaspx = "ApplicationReview.aspx";
-
-        public int currentPositionID
-        {
-            get
-            {
-                CheckQueryString();
-
-                //Try parsing the PositionID query string into an integer.
-                //positionID will be number represented in the string or 0 on failure
-                int positionID = 0;
-                int.TryParse(Request.QueryString[STR_PositionID], out positionID);
-
-                return positionID;
-            }
-        }
-
-        public Position currentPosition
-        {
-            get
-            {
-                return PositionBLL.GetByID(currentPositionID);
-            }
-        }
-
-        protected void Page_Init(object sender, EventArgs e)
-        {
-        }
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (!IsPostBack)
-            {
-                litPositionTitle.Text = currentPosition.PositionTitle;
-            }
-        }
-
-        public string GetNullSafeFullName(string FullName)
-        {
-            return ApplicantBLL.GetNullSafeFullName(FullName);
-        }
-
-        /// <summary>
-        /// Check to ensure that they querystring "PositionID" is not null or empty
-        /// </summary>
-        private void CheckQueryString()
-        {
-            if (string.IsNullOrEmpty(Request.QueryString[STR_PositionID]))
-                Response.Redirect(RecruitmentConfiguration.ErrorPage(RecruitmentConfiguration.ErrorType.UNKNOWN));
-        }
-
-        protected void ObjectDataApplications_Selecting(object sender, ObjectDataSourceSelectingEventArgs e)
-        {
-            e.InputParameters["position"] = currentPosition;
-        }
-
-        /// <summary>
-        /// Redirect to the applicationReview page for this application
-        /// </summary>
-        /// <param name="sender">Sender has a command argument with the applicationID</param>
-        protected void lbtnViewApplication_Click(object sender, EventArgs e)
-        {
-            string applicationID = ((LinkButton)sender).CommandArgument;
-
-            Response.Redirect(string.Format("{0}?ApplicationID={1}", STR_ApplicationReviewaspx, applicationID));
-        }
-}
-
+        
+    }
 }
