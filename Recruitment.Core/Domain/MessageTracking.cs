9e593f679231a3e4e9481b08cf3d4d9e697fc409
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:17:01 +0000

    Added a new MessageTracking class to recruitments that will be used to track emails sent by the system (mostly those to applicants). (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4148. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3255

---
+++ Recruitment.Core/Domain/MessageTracking.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+namespace CAESDO.Recruitment.Core.Domain
+{
+    public class MessageTracking : DomainObject<MessageTracking,int>
+    {
+        [NotNullValidator]
+        [StringLengthValidator(1, 50)]
+        public virtual string To { get; set; }
+
+        [NotNullValidator]
+        [StringLengthValidator(1, 50)]
+        public virtual string From { get; set; }
+
+        [NotNullValidator]
+        [StringLengthValidator(1, 50)]
+        public virtual string SentBy { get; set; }
+
+        [NotNullValidator]
+        [StringLengthValidator(1, 50)]
+        public virtual string Body { get; set; }
+
+        [NotNullValidator] //always not null since its a datetime
+        public virtual DateTime DateSent { get; set; }
+
+    }
+}
---
+++ Recruitment.Test/DomainTests/MessageTrackingTest.cs
@@ -0,0 +1,30 @@
+﻿using CAESDO.Recruitment.BLL;
+using CAESDO.Recruitment.Core.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace CAESDO.Recruitment.Test.DomainTests
+{
+    [TestClass]
+    public class MessageTrackingTest : DatabaseTestBase
+    {
+        [TestMethod]
+        public void CanSaveMessageTrackingEntry()
+        {
+            var tracking = new MessageTracking
+                               {
+                                   From = StaticProperties.TestString,
+                                   To = StaticProperties.TestString,
+                                   SentBy = StaticProperties.TestString,
+                                   Body = StaticProperties.TestString,
+                                   DateSent = DateTime.Now
+                               };
+
+            using (var ts = new TransactionScope())
+            {
+                GenericBLL<MessageTracking, int>.EnsurePersistent(tracking);
+   
+                ts.CommitTransaction();
+            }
+        }
+    }
+}
