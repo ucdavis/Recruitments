73468f6e5d732805214cac2e5c656a3823121dd1
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:05:44 +0000

    Created and updated resources for all of the template fields in the position management page, including removing the old static string code-behind confidentiality statement. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 2963. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C2941

--- Recruitment.Web/Authorized/PositionManagement.aspx.cs
+++ Recruitment.Web/Authorized/PositionManagement.aspx.cs
@@ -534,13 +534,5 @@ namespace CAESDO.Recruitment.Web
             }
             return output;
         }
-
-        /// <summary>
-        /// Returns the confidentiality statement
-        /// </summary>
-        public string ConfidentialityStatement()
-        {
-            return "Confidentiality Statement";
-        }
 }
 }
