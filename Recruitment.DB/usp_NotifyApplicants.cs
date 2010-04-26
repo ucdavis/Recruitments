f91002dabd5623a5b6342e89755f1cdaaa11e9e1
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 20:55:28 +0000

    Updated the NotifyApplicants SPROC to gather a list of applicants using a List<UserInfo> and then sending each an Email using the SQL Server 2005 dbmail system SPROC.  Note: Database mail needs to be setup on the server for this to work. (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 2335. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C2653

--- Recruitment.DB/usp_NotifyApplicants.cs
+++ Recruitment.DB/usp_NotifyApplicants.cs
@@ -3,7 +3,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
-
+using System.Net.Mail;
+using System.Collections.Generic;
 
 public partial class StoredProcedures
 {
@@ -20,18 +21,74 @@ public partial class StoredProcedures
 
         con.Open();
 
+        List<UserInfo> users = new List<UserInfo>();
+
         using (SqlDataReader rdr = cmd.ExecuteReader())
         {
             while (rdr.Read())
             {
+                users.Add(new UserInfo(rdr));
+
                 SqlContext.Pipe.Send(rdr["Email"].ToString());
             }
 
             //SqlContext.Pipe.Send(rdr);
         }
 
+        
+        SqlCommand sendEmail = new SqlCommand("", con);
+
+        foreach (UserInfo user in users)
+        {
+            sendEmail.CommandText = string.Format("EXEC msdb.dbo.sp_send_dbmail @recipients='{0}', @subject='{1}', @body = '{2}', @body_format = '{3}'",
+                                            "srkirkland@ucdavis.edu", //user.Email, 
+                                            "Subject", 
+                                            "Hello: " + user.FullName, 
+                                            "HTML");
+            sendEmail.ExecuteNonQuery();
+        }
+        
         con.Close();
 
         // Put your code here
+    }
+
+    public class UserInfo
+    {
+        private string _Email;
+
+        public string Email
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
+
+        private string _FirstName;
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+
+        private string _LastName;
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        public UserInfo(SqlDataReader rdr)
+        {
+            Email = rdr["Email"].ToString();
+            FirstName = rdr["FirstName"].ToString();
+            LastName = rdr["LastName"].ToString();
+        }
     }
 };
