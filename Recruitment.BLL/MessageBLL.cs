b7bf489e07e7c9d96b05c685b472a6d73f906413
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 21:17:53 +0000

    Replaced the DevMessageGateway with the proper implementation of the MessageGateway. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4452. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C3312

--- Recruitment.BLL/MessageBLL.cs
+++ Recruitment.BLL/MessageBLL.cs
@@ -7,7 +7,7 @@ namespace CAESDO.Recruitment.BLL
 {
     public class MessageBLL
     {
-        public static IMessageGateway MessageGateway = new DevMessageGateway();
+        public static IMessageGateway MessageGateway = new MessageGateway();
         public static IPrincipal UserContext = new UserContext();
 
         /// <summary>
