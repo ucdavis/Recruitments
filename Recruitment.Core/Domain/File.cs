fe06762db5cc2ff655921e2b11102310b9b097bb
SU for Alan Lai <srkirkland@gmail.com>
2010-04-26 20:52:47 +0000

    (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 2199. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Recruitments/Trunk/Recruitment;C2584

--- Recruitment.Core/Domain/File.cs
+++ Recruitment.Core/Domain/File.cs
@@ -41,19 +41,28 @@ namespace CAESDO.Recruitment.Core.Domain
         {
         }
 
-        public virtual void Fill()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path">Path to file root folder</param>
+        /// <returns></returns>
+        public virtual System.IO.FileInfo GetFileData(string path)
         {
-            throw new System.NotImplementedException();
-        }
+            System.IO.FileInfo fileToDownload = new System.IO.FileInfo(path + this.ID);
 
-        public virtual void Save()
-        {
-            throw new System.NotImplementedException();
-        }
+            return fileToDownload;
+            //if (fileToDownload.Exists)
+            //{
+            //    Response.Clear();
 
-        public virtual byte[] GetFileData()
-        {
-            throw new System.NotImplementedException();
+            //    //Control the name that they see
+            //    Response.ContentType = "application/octet-stream";
+            //    Response.AddHeader("Content-Disposition", "attachment;filename=" + HttpUtility.UrlEncode(fileName));
+            //    Response.AddHeader("Content-Length", fileToDownload.Length.ToString());
+            //    //Response.TransmitFile(path + FileID.ToString());
+            //    Response.TransmitFile(fileToDownload.FullName);
+            //    Response.End();
+            //}
         }
 
         public virtual byte[] ConvertFile(byte[] FileToConvert)
